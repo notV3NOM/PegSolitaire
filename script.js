9ba9b7c1dd6b8b889a3e84c6b872ead35956efef
notV3NOM <123deeznuts456789@email.com>
2023-11-22 18:18:26 +0530

    Add player

--- script.js
+++ script.js
@@ -21,8 +21,27 @@ for (i = 0; i < boardSize; ++i) {
 // Prepare the board with elements
 for (i = 0; i < boardSize; ++i) {
   for (j = 0; j < boardSize; ++j) {
+    // Skip center of the board
+    if (i == Math.floor(boardSize / 2) && j == Math.floor(boardSize / 2)) {
+      continue;
+    }
+
+    // Add player piece
+    if (i == Math.floor(boardSize / 2) + 2 && j == Math.floor(boardSize / 2)) {
+      var player = document.createElement("span");
+      player.className = "player";
+      document.getElementById("" + i + j).appendChild(player);
+      continue;
+    }
+
     var peg = document.createElement("span");
     peg.className = "peg";
     document.getElementById("" + i + j).appendChild(peg);
   }
 }
+
+// Function to perform Move
+// Takes the current player position and a cell to be moved to
+
+// Function to return valid Moves
+// Takes the current player position and returns a list of cells to which player can move
